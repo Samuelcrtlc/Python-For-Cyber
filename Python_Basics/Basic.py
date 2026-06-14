88a084117f87ee2d5e1d8e904af449db3eb353aa
Samuel Natalicio da Silva <samuelnatalicio22@gmail.com>
2026-06-14 00:44:22 -0300

    A basic code using a class and a function

---
+++ Python_Basics/Basic.py
@@ -0,0 +1,15 @@
+class Solution:
+    def somaPares(self, nums: list[int]) -> int:
+        soma = 0                    # Create the variable to accumulate the total / Criamos a variável para acumular o total 
+        for i in nums:
+            if i % 2 == 0:          # If the remainder of the division by 2 is 0, it's even! / Se o resto da divisão por 2 for 0, ele é par!
+
+                soma += i           # Add the current number to our total / Somamos o número atual ao nosso total 
+        return soma                 # At the end, we return the accumulated result / No final, retornamos o resultado acumulado
+
+solicitador = Solution()            
+
+lista_teste = [1, 2, 3, 4, 5, 6]
+
+resultado = solicitador.somaPares(lista_teste)
+print(f"A soma dos números pares é: {resultado}")
---
+++ Python_Basics/README.md
@@ -0,0 +1,8 @@
+# In English
+
+### This module covers core programming logic, data structures, and control flow challenges, establishing a solid foundation in Python.
+
+
+# Em Português
+
+### Este módulo abrange desafios de lógica de programação, estruturas de dados e controle de fluxo, estabelecendo uma base sólida em Python.
